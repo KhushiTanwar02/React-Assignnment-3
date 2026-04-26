f456adfe988b178501964c94fe93af1fc159b4ef
khushi <khushitanwar0005@gmail.com>
2026-04-26 20:59:15 +0530

    Assignmemt-3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/React-Assignment-3/',
+  base: '/React-Assignnment-3/',
   plugins: [react()],
 })
